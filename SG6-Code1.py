0d12c8f55804cf0289b0b32289e4bbe29edb734a
psalmlee-amats <psalm.lee@smc.pshs.edu.ph>
2025-09-09 10:40:10 +0800

    number rounder
    It rounds the total of three decimal numbers to only 2 decimal points.

---
+++ SG6-Code1.py
@@ -0,0 +1,8 @@
+num1 = float(input('Enter first number:'))
+num2 = float(input('Enter second number:'))
+num3 = float(input('Enter third number:'))
+
+total = (num1 + num2 + num3)
+number_rounded = round(total, 2)
+
+print(number_rounded)
